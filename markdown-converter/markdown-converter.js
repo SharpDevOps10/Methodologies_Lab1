628530f21401b115cc0cb6ea11ac1b91cd6aea44
SharpDevOps10 <td090833@gmail.com>
2024-03-02 23:32:25 +0200

    feature: added enhanced handling of preformatted text blocks
    - implemented the functionality for processing preformatted text blocks using triple backticks
    - ensured proper replacement of preformatted text in the HTML

--- markdown-converter/markdown-converter.js
+++ markdown-converter/markdown-converter.js
@@ -8,6 +8,15 @@ const TagMap = {
 
 const paragraphOpeningTag = '<p>';
 const paragraphClosingTag = '</p>';
+const backtick = '```';
+const preformattedBlockOpeningTag = '<pre>\n';
+const preformattedBlockClosingTag = '</pre>\n';
+
+function processPreformattedBlock(result, isPreformattedBlock) {
+  isPreformattedBlock[0] = !isPreformattedBlock[0];
+  if (isPreformattedBlock[0]) result.push(preformattedBlockOpeningTag);
+  else result.push(preformattedBlockClosingTag);
+}
 
 function processParagraph(result, isParagraphOpen, line) {
   const trimmedLine = line.trim();
@@ -35,11 +44,17 @@ function convertMarkdownToHTML(markdown) {
   const isParagraphOpen = [false];
 
   for (const line of lines) {
+    if (line.startsWith(backtick)) {
+      processPreformattedBlock(result, isPreformattedBlock);
+      continue;
+    }
     if (isPreformattedBlock[0]) result.push(line + '\n');
     else processParagraph(result, isParagraphOpen, line);
   }
 
   if (isParagraphOpen[0]) result.push(paragraphClosingTag + '\n');
+
+  if (isPreformattedBlock[0]) result.push(preformattedBlockClosingTag);
 
   return result.join('');
 }
